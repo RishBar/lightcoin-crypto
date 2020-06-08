5c3ba086db431f4571bd8629ddf637807f44444d
Rishav Baral <rishavbaral6@gmail.com>
2020-06-08 03:11:47 +0000

    implement the program

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
-let balance = 500.00;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+    // this.balance = 0;
+  }
+  get balance() {
+    let num = 0;
+    for (let transaction of this.transactions) {
+      num += transaction.value();
+    }
+    return num;
+  }
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
 
-class Withdrawal {
-
-  constructor(amount) {
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
-
   commit() {
-    balance -= this.amount;
+    if (this.value() < 0 && this.account.balance < this.amount) {
+      console.log('u aint got no funds b');
+    } else {
+      this.time = new Date();
+      this.account.addTransaction(this);
+    }
   }
 
+}
+
+class Withdrawal extends Transaction {
+
+  value() {
+    return (this.amount * -1);
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  value() {
+    return this.amount;
+  }
 }
 
 
@@ -18,13 +53,30 @@ class Withdrawal {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+let a1 = new Account('Rbarz');
+// console.log(a1);
+
+
+let t1 = new Deposit(120.00, a1);
 t1.commit();
-console.log('Transaction 1:', t1);
+console.log(a1.balance);
 
-t2 = new Withdrawal(9.99);
+
+let t2 = new Withdrawal(20.00, a1);
 t2.commit();
-console.log('Transaction 2:', t2);
+console.log(a1.balance);
 
-console.log('Balance:', balance);
-let a = 1;
+
+
+
+// console.log(a1.transaction);
+
+// let t1 = new Withdrawal(50.25);
+// t1.commit();
+// console.log('Transaction 1:', t1);
+
+// let t2 = new Withdrawal(9.99);
+// t2.commit();
+// console.log('Transaction 2:', t2);
+
+// console.log('Balance:', balance);
